51f7406fbbaacdb8ae37c9a9ad960f1d0303695b
Dave Sieh <davesieh@gmail.com>
2021-02-09 22:54:42 +0000

    Now reading pdf words from file rather than stdout.

--- test/indexSpec.js
+++ test/indexSpec.js
@@ -10,6 +10,19 @@ const pdfFile = 'InnocuousDocument.pdf'
 const pdfDir = 'test-data'
 const pdfDoc = path.join(pdfDir, pdfFile)
 const pngDoc = path.join(pdfDir, 'InnocuousDocument.png')
+const pdfWordsNoOptions = path.join(pdfDir, 'InnocuousDocument.txt')
+const pdfWordsJustGrep = path.join(pdfDir, 'InnocuousDocumentGrep.txt')
+const pdfWordsGrepNoNewline = path.join(pdfDir, 'InnocuousDocumentGrepNoNewline.txt')
+const pdfWordsNoNewlinePage1 = path.join(pdfDir, 'InnocuousDocumentNoNewlinePage1.txt')
+
+const readFile = (filePath) => {
+  return new Promise((resolve, reject) => {
+    fse.readFile(filePath, 'utf8', (err, data) => {
+      if (err) return reject(err)
+      resolve(data)
+    })
+  })
+}
 
 describe('pdf-tools', function () {
   describe('#splitDocument', function () {
@@ -164,39 +177,59 @@ describe('pdf-tools', function () {
 
   describe('#pdfWords', function () {
     it('returns the text from the PDF with no filtering and with newlines', function (done) {
-      tools.pdfWords(pdfDoc)
-        .then(function (text) {
-          expect(text.length).to.not.equal(0)
-          done()
+      readFile(pdfWordsNoOptions)
+        .then((expectedText) => {
+          tools.pdfWords(pdfDoc)
+            .then(function (text) {
+              expect(text.length).to.not.equal(0)
+              expect(text).to.equal(expectedText)
+              done()
+            })
+            .catch(done)
         })
         .catch(done)
     })
 
     it('returns the text from the PDF with filtering and with no newlines', function (done) {
       let textFilterRegex = '[^ ",]{3,}'
-      tools.pdfWords(pdfDoc, textFilterRegex)
-        .then(function (text) {
-          expect(text.length).to.not.equal(0)
-          done()
+      readFile(pdfWordsJustGrep)
+        .then((expectedText) => {
+          tools.pdfWords(pdfDoc, textFilterRegex)
+            .then(function (text) {
+              expect(text.length).to.not.equal(0)
+              expect(text).to.equal(expectedText)
+              done()
+            })
+            .catch(done)
         })
         .catch(done)
     })
 
     it('returns the text from the PDF with filtering and no newlines', function (done) {
       let textFilterRegex = '[^ ",]{3,}'
-      tools.pdfWords(pdfDoc, textFilterRegex, true)
-        .then(function (text) {
-          expect(text.length).to.not.equal(0)
-          done()
+      readFile(pdfWordsGrepNoNewline)
+        .then((expectedText) => {
+          tools.pdfWords(pdfDoc, textFilterRegex, true)
+            .then(function (text) {
+              expect(text.length).to.not.equal(0)
+              expect(text).to.equal(expectedText)
+              done()
+            })
+            .catch(done)
         })
         .catch(done)
     })
 
     it('will extract the text of only the first page of a PDF', function (done) {
-      tools.pdfWords(pdfDoc, null, true, 1, 1)
-        .then(function (text) {
-          expect(text.length).to.not.equal(0)
-          done()
+      readFile(pdfWordsNoNewlinePage1)
+        .then((expectedText) => {
+          tools.pdfWords(pdfDoc, null, true, 1, 1)
+            .then(function (text) {
+              expect(text.length).to.not.equal(0)
+              expect(text).to.equal(expectedText)
+              done()
+            })
+            .catch(done)
         })
         .catch(done)
     })
